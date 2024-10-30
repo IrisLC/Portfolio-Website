ffd936bac6a10fbf06cbd29f9ecc10752489e163
curriejl <curriejl@miamioh.edu>
2024-10-30 13:51:17 -0400

    added pages for maya works and strange tower

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 let button1 = document.getElementById("head-one");
 let button2 = document.getElementById("head-two");
 
-button1.addEventListener("click", open1);
-button2.addEventListener("click", open2);
+if (button1 !== null) {
+  button1.addEventListener("click", open1);
+  button2.addEventListener("click", open2);
+}
 
 function open1() {
   let answer = document.getElementById("drop-one");
   button1.classList.toggle("openPlus");
-  answer.classList.toggle("open");
+  answer.classList.toggle("open1");
 }
 
 function open2() {
   let answer = document.getElementById("drop-two");
   button2.classList.toggle("openPlus");
-  answer.classList.toggle("open");
+  answer.classList.toggle("open2");
+}
+
+let d1 = document.getElementById("dorsey-1");
+let d2 = document.getElementById("dorsey-2");
+let d3 = document.getElementById("dorsey-3");
+
+d1.addEventListener("click", () => increaseImage(d1));
+d2.addEventListener("click", () => increaseImage(d2));
+d3.addEventListener("click", () => increaseImage(d3));
+
+function increaseImage(d) {
+  console.log(d);
+  d.classList.toggle("large-img");
 }
